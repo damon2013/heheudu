11c5258ae8bc6e7b3c3ae79320af84223f4034a8
liuxukun <liuxukun@chinasofti.com>
2017-04-18 00:05:31 +0800

    首次提交

---
+++ Fade/Fade/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fade
+//
+//  Created by liuxukun on 17/4/17.
+//  Copyright © 2017年 liuxukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fade/Fade/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fade
+//
+//  Created by liuxukun on 17/4/17.
+//  Copyright © 2017年 liuxukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
